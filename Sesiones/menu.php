5ce937714e0e74659c67673a89deca3cf6977b17
fibionacho <nabifo@hotmail.com>
2022-12-19 17:56:04 +0100

    ejSesiones

---
+++ Sesiones/menu.php
@@ -0,0 +1,21 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="menu">
+        <a href="privado1.php">privada1</a>
+        <a href="privado2.php">privada2</a>
+        <a href="privado3.php">privada3</a>
+        <a href="login.php">login</a>
+        <a href="logout.php">logout</a>
+    </div>
+</body>
+</html>
