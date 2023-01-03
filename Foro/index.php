7fb4c4e4700c749f5498f7fafa6405a1ddb9f110
fibionacho <nabifo@hotmail.com>
2023-01-03 11:05:12 +0100

    primeroSoloEstilo

---
+++ Foro/index.php
@@ -0,0 +1,32 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="estilos/principal.css">
+</head>
+
+<body>
+    <div id="margen">
+        <div id="navegador">
+            <p id="logo">Foro Romano</p>
+            <div id="enlace">
+                <a href="iniciar.php" class="enlace">Iniciar sesión</a>
+                <a href="registro.php" class="enlace">Registrarse</a>
+            </div>
+        </div>
+        <h1>Probando el foro, la página de inicio poco más debería tener que esto, no?</h1>
+        <h2>Foro para discutir sobre foros. Especialistas en construcciones greco-latinas</h2>
+        <div id="imagen">
+            <img src="estilos/roman-forum.jpg" alt="foro romano" width="100%" height="50%">
+        </div>
+    </div>
+</body>
+
+</html>
