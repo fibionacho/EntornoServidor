230e8b591a507fda0efb49c3c59c26a625fa50b0
fibionacho <nabifo@hotmail.com>
2023-01-03 20:23:35 +0100

    ++

--- Foro/iniciar.php
+++ Foro/iniciar.php
@@ -8,8 +8,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio</title>
+    <title>Iniciar Sesión</title>
     <link rel="stylesheet" href="estilos/principal.css">
+    <link rel="stylesheet" href="estilos/iniciar.css">
 </head>
 
 <body>
@@ -19,13 +20,21 @@
             <p id="logo">Foro Romano</p>
             <div id="enlace">
                 <a href="iniciar.php" class="enlace">Iniciar sesión</a>
-                <a href="registro.php" class="enlace">Registrarse</a>
+                <a href="index.php" class="enlace">Registrarse</a>
             </div>
         </div>
-        <h1>Probando el foroaaaaaa, la página de inicio poco más debería tener que esto, no?</h1>
+        <h1>Página de inicio de sesión</h1>
         <h2>Foro para discutir sobre foros. Especialistas en construcciones greco-latinas</h2>
-        <div id="imagen">
-            <img src="estilos/roman-forum.jpg" alt="foro romano" width="100%" height="50%">
+        <div id="inicioSesion">
+            <form action="" method="post">
+                <label for="nombre">Introduzca el nombre del usuario</label>
+                <br>
+                <input type="text" name="nombre" id="nombre">
+                <br>
+                <label for="contrasena">Introduzca su contraseña</label>
+                <br>
+                <input type="password" name="contrasena" id="contrasena">
+            </form>
         </div>
     </div>
 </body>
